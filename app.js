f961047a70eb0b34ecdf3081f03c1fd64085d965
Dawid Glapiak <dg18@live.it>
2022-02-12 13:51:42 +0100

    timer completed

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const daysEl = document.querySelector('#days');
+const hoursEl = document.querySelector('#hours');
+const minutesEl = document.querySelector('#minutes');
+const secondsEl = document.querySelector('#seconds');
+
 const fuerteTrip = '15 February 2022 9:10';
 
 function countdown(){
@@ -11,8 +16,15 @@ function countdown(){
     const minutes = Math.floor((totalSeconds / 60) %60) ;
     const seconds = Math.floor(totalSeconds % 60);
 
-    console.log(days, hours, minutes, seconds);
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML =  formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
     
+}
+
+function formatTime(time){
+   return time < 10 ? (`0${time}`) : time;
 }
 
 //initial call
